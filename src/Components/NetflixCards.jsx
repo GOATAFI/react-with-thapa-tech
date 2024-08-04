9f095f5feb07b9fc524ca7c3d2956185e8c00576
GOATAFI <projectexecutiveofpm@gmail.com>
2024-08-04 21:21:54 +0600

    netflix cards with props

--- src/Components/NetflixCards.jsx
+++ src/Components/NetflixCards.jsx
@@ -1,38 +1,10 @@
 import seriesData from "../api/seriesData.json";
+import { SeriesCard } from "./SeriesCard";
 export const NetflixCards = () => {
-  // const name = "Game of Thrones";
-  // const imageLink =
-  //   "https://m.media-amazon.com/images/M/MV5BN2IzYzBiOTQtNGZmMi00NDI5LTgxMzMtN2EzZjA1NjhlOGMxXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg";
-  // const description =
-  //   "Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being dormant for millennia.";
-  // const returnGenre = () => {
-  //   const genre = "Action";
-  //   return genre;
-  // };
-  // const rating = 8.9;
-  // const age = 18;
-  // const canWatch = () => {
-  //   if (age >= 18) return "Watch Now";
-  //   return "Not Available";
-  // };
   return (
     <>
       {seriesData.map((curElem) => {
-        return (
-          <div key={curElem.id}>
-            <div>
-              <img src={curElem.img_url} alt="" width="400px" height="400px" />
-            </div>
-            <h2>Name : {curElem.name}</h2>
-            <h4> Rating : {curElem.rating}</h4>
-            <p>Description : {curElem.description}</p>
-            <p>Genre : {curElem.genre}</p>
-            <p>Cast : {curElem.cast}</p>
-            <a href={curElem.watch_url}>
-              <button>Watch Now</button>
-            </a>
-          </div>
-        );
+        return <SeriesCard key={curElem.id} curElem={curElem} />;
       })}
     </>
   );
---
+++ src/Components/SeriesCard.jsx
@@ -0,0 +1,19 @@
+export const SeriesCard = ({ curElem }) => {
+  const { img_url, name, rating, description, genre, cast, watch_url } =
+    curElem;
+  return (
+    <>
+      <div>
+        <img src={img_url} alt="" width="400px" height="400px" />
+      </div>
+      <h2>Name : {name}</h2>
+      <h4> Rating : {rating}</h4>
+      <p>Description : {description}</p>
+      <p>Genre : {genre}</p>
+      <p>Cast : {cast}</p>
+      <a href={watch_url}>
+        <button>Watch Now</button>
+      </a>
+    </>
+  );
+};
